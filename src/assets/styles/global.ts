a35fea5a81b9b3fca9f6a8e4173a99d37d219d90
Renan Moreira <renan.bcc@gmail.com>
2022-06-18 18:14:38 -0300

    Reset e estilos globais

---
+++ src/assets/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #F0F2F5;
+    --red: #e52e4d;
+    --blue: #5429CC;
+    --green: #33cc95;
+    
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --shape: #FFFFFF;
+    --white: #FFFFFF;
+  }
+
+  html {
+    // font-size padrão para desktop é 16px
+    // 1 REM = 16px;
+
+    @media (max-width: 992px) {
+      // 16x0.9375 = 15px
+      // 1 REM = 15px;
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 768px) {
+      // 16x0.875 = 14px
+      // 1 REM = 14px;
+      font-size: 87.5%;
+    }
+
+    /**
+    * Todas as medidas do sistema será em REM.
+    * REM usa o tamanho da font-size como base.
+    * 
+    *
+    * Configurando medidas adequadas para telas desktop, tablet e smartfone
+    * garantimos que todos os elementos que recebe algum tipo
+    * de medida em REM seja redimencionado na mesma proporção.
+    */
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { GlobalStyle } from './assets/styles/global';
+import { GlobalReset } from './assets/styles/reset';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalReset />
+    <GlobalStyle />
   </React.StrictMode>
 );
