b3bda1669ad10efb4006898398299223ce9d719f
Renan Moreira <renan.bcc@gmail.com>
2022-06-18 20:17:37 -0300

    Component Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 
 export function App() {
   return (
     <div className="App">
       <Header />
+      <Dashboard />
     </div>
   );
 }
