c115f288f4c21f19767674f04ab01ad14ac7eefe
Renan Moreira <renan.bcc@gmail.com>
2022-06-18 17:47:53 -0300

    Adicionando styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-export default function App() {
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: blue;
+`
+
+export function App() {
   return (
     <div className="App">
-      <h1>Money - Gerenciamento Financeiro</h1>
+      <Title>Money - Gerenciamento Financeiro</Title>
     </div>
   );
 }
