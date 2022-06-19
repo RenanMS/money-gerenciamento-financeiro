7cade4f30611e3860efae8f9cddb7f71d9647cbd
Renan Moreira <renan.bcc@gmail.com>
2022-06-19 19:49:54 -0300

    Criando API no Front-end

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { GlobalStyle } from './assets/styles/global';
 import { GlobalReset } from './assets/styles/reset';
+import { mirageJS } from './services/miragejs';
+
+mirageJS()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
