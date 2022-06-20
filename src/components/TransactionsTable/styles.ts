6346c37e204b6cf6593e5b4eda78146c3732fa5a
renan <renan@methodostecnologia.com.br>
2022-06-20 17:44:15 -0300

    Responsivando Tabela

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -1,11 +1,18 @@
 import styled from "styled-components";
+import { respond } from "../../util/StyledBreakpointRespond";
 
 export const Container = styled.div`
   margin-top: 4rem;
 
+  ${respond.below('md', {
+     marginTop: '2rem'
+  })}
+
   table {
     width: 100%;
     border-spacing: 0 0.5rem;
+    border-collapse: initial;
+    font-size: 1rem;
 
     th {
       color: var(--text-body);
@@ -20,10 +27,27 @@ export const Container = styled.div`
       border: 0;
       background: var(--shape);
       color: var(--text-body);
-      border-radius: 0.25rem;
 
       &:first-child {
         color: var(--text-tittle);
+        border-bottom-left-radius: 1rem;
+        border-top-left-radius: 1rem;
+
+        ${respond.below('md', {
+          borderTopRightRadius: '1rem',
+          borderBottomLeftRadius: '0rem',
+        })}
+      }
+
+      &:last-child {
+        border-bottom-right-radius: 1rem;
+        border-top-right-radius: 1rem;
+
+        ${respond.below('md', {
+          borderBottom: 'none',
+          borderTopRightRadius: '0rem',
+          borderBottomLeftRadius: '1rem',
+        })}
       }
 
       &.deposit {
@@ -32,6 +56,56 @@ export const Container = styled.div`
 
       &.withdraw {
         color: var(--red);
+      }
+    }
+  }
+
+  table.reponsive.card {
+    table-layout: fixed;
+
+    ${respond.below('md', {
+      border: '0'
+    })}
+
+    thead {
+      ${respond.below('md', {
+        border: 'none',
+        clip: 'rect(0 0 0 0)',
+        height: '1px',
+        margin: '-1px',
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        width: '1px'
+      })}
+    }
+
+    tr {
+      ${respond.below('md', {
+        display: 'block',
+        marginBottom: '1.4rem'
+      })}
+    }
+
+    td {
+        ${respond.below('md', {
+        display: 'block',
+        borderBottom: '1px solid var(--background)',
+        textAlign: 'right'
+      })}
+
+      &:before {
+        ${respond.below('md', {
+          /*
+          * aria-label has no advantage, it won't be read inside a table
+          content: attr(aria-label);
+          */
+          content: 'attr(data-label)',
+          float: 'left',
+          textTransform: 'uppercase',
+          fontSize: '.9rem',
+          color: 'var(--text-body)'
+        })}
       }
     }
   }
